9a03bdb7fe5f71f616f37baa6b6fae6ec4a4e521
NehanSikder <arham.sikder@gmail.com>
2024-06-20 22:52:29 -0400

    Added reset button

--- src/App.js
+++ src/App.js
@@ -11,6 +11,8 @@ function App() {
   const [gameConfigured, setGameConfigured] = useState(false)
   const [players, setPlayers] = useState([]);
   const [subtract, setSubtract] = useState(false);
+  const [showReset, setShowReset] = useState(false);
+  const [confirmReset, setConfirmReset] = useState(false);
 
 
   function getGameSettings(numberOfPlayers, startingScore, subtract){
@@ -21,6 +23,7 @@ function App() {
     }
     setPlayers(tmp)
     setSubtract(subtract)
+    setShowReset(true)
   }
 
   function updatePlayer(id, score){
@@ -41,12 +44,47 @@ function App() {
       setPlayers(playersCopy)
   }
 
+  const handleReset = () =>{
+    setConfirmReset(!confirmReset)
+    setShowReset(false)
+
+  }
+
+  const cancelReset = () => {
+    setConfirmReset(false)
+    setShowReset(true)
+  }
+
+  const reset = () => {
+    setConfirmReset(false)
+    setGameConfigured(false)
+    setShowReset(false)
+  }
+
+  const renderConfirm = () => {
+    return  (
+      <div class="flex space-x-2">
+        <button class="mb-2 bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" onClick={handleReset}>Reset</button>
+        <span class="align-center">Are you sure? </span>
+        <button class="mb-2 bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" onClick={reset}>Yes</button>
+        <button class="mb-2 bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" onClick={cancelReset}>No</button>
+      </div>
+    )
+  }
+
   return (
     <div className="App">
       <div class="flex justify-center items-center h-screen bg-orange-300">
         <div>
-          <h1 class="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">Score Tracker</h1> 
+          {!showReset && <h1 class="mb-1 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">Score Tracker</h1>}
           {!gameConfigured && <GameSettingsForm getGameSettings={getGameSettings}/>}
+          {showReset && <div>
+            <h1 class="mb-1 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">Score Tracker</h1>
+            <button class="mb-2 bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" onClick={handleReset}>Reset</button>
+            
+            </div>}
+          {confirmReset && renderConfirm()}
+          
           {gameConfigured && <ScoreBoard players={players} updatePlayer={updatePlayer}/>}
         </div>
       </div>
